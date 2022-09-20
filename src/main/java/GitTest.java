04c0582a44bc7b20ce130591118d2c361b122822
t <t@t>
2022-09-20 12:08:55 +0800

    gitTest增加了main函数

--- src/main/java/GitTest.java
+++ src/main/java/GitTest.java
@@ -1,2 +1,7 @@
 public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello world！");
+        System.out.println("hello world！");
+        System.out.println("hello world！");
+    }
 }
